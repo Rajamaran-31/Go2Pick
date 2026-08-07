1c1f597dac37f2ba8fa8a84a457c920f4431a74e
T Rajamaran <rajamaran32@gmail.com>
2026-08-07 20:58:55 +0530

    config: allow dynamic VITE_API_URL from environment

--- admin-panel/src/services/api.js
+++ admin-panel/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { auth } from '../firebase';
 
-export const API_BASE = 'http://localhost:8000';
+export const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 const api = axios.create({
   baseURL: API_BASE,
